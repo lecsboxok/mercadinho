3f2462e1160e239825b8655d9ac20802ded112f5
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2024-05-11 22:53:08 -0300

    .

--- screens/carrinho.js
+++ screens/carrinho.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function Carrinho() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      <Image
-        source={require("../images/logo.png")}
-      />
-      <Text>Carrinho</Text>
-    </View>
+      <View style={styles.tudo}>
+        <StatusBar style="auto" />
+        <View style={styles.cabecalho}>
+          <Image source={require('../images/logo.png')} style={styles.image} />
+          <MaterialCommunityIcons name="account" color="#CFCFCF" size={30} style={styles.icon} />
+        </View>
+        <View style={styles.titulo}>
+          <Text>Meu carrinho</Text>
+        </View>
+      </View >
+    </View >
   );
 }
 
@@ -18,7 +23,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FCFAF1',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  tudo: {
+    margin: 30
+  },
+  image: {
+    width: 50,
+    height: 35.2,
+    marginTop: 20,
+    marginRight: 'auto',
+  },
+  icon: {
+    marginLeft: 'auto',
+    marginTop: 20,
+    marginRight: 10,
+  },
+  cabecalho: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  },
+  titulo:{
+    color: '#3F3F3F',
+    fontSize: 18,
+  }
 });
