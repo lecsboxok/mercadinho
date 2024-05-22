ca9470187ade39d636665a516b7e30c7aeec62db
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2024-05-22 10:34:47 -0300

    .

--- screens/categoria.js
+++ screens/categoria.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export function Categoria() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.titulo}>Categoria</Text>
       <View style={styles.tudo}>
@@ -11,15 +11,15 @@ export function Categoria() {
           <Text style={styles.tituloCat}>GRÃOS E MASSAS</Text>
           <Image source={require('../images/graos.png')} style={styles.image} />
         </View>
-        <View style={styles.retangulo1}>
+        <View style={styles.retangulo2}>
           <Text style={styles.tituloCat}>AÇOUGUE</Text>
           <Image source={require('../images/acougue.png')} style={styles.image} />
         </View>
-        <View style={styles.retangulo1}>
+        <View style={styles.retangulo3}>
           <Text style={styles.tituloCat}>BEBIDAS</Text>
           <Image source={require('../images/bebidas.png')} style={styles.image} />
         </View>
-        <View style={styles.retangulo1}>
+        <View style={styles.retangulo4}>
           <Text style={styles.tituloCat}>HORTIFRUTI</Text>
           <Image source={require('../images/frutas.png')} style={styles.image} />
         </View>
@@ -35,18 +35,30 @@ export function Categoria() {
           <Text style={styles.tituloCat}>PADARIA</Text>
           <Image source={require('../images/padaria.png')} style={styles.image} />
         </View>
+        <View style={styles.retangulo1}>
+          <Text style={styles.tituloCat}>CONGELADOS</Text>
+          <Image source={require('../images/congelados.png')} style={styles.image} />
+        </View>
+        <View style={styles.retangulo1}>
+          <Text style={styles.tituloCat}>BISCOITOS E DOCES</Text>
+          <Image source={require('../images/biscoitos.png')} style={styles.image} />
+        </View>
+        <View style={styles.retangulo1}>
+          <Text style={styles.tituloCat}>MERCARIA</Text>
+          <Image source={require('../images/mercearia.png')} style={styles.image} />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
   titulo: {
     fontSize: 20,
     fontWeight: 'medium',
@@ -57,16 +69,50 @@ const styles = StyleSheet.create({
     fontFamily: 'PoppinsMedium',
   },
   tudo: {
-    margin: 20
+    margin: 50
+  },
+  tituloCat:{
+    color: '#FFF',
+    fontSize: 18
+  },
+  image:{
+    width: 50,
+    height: 50
   },
   retangulo1:{
     backgroundColor: '#E29D6C',
     alignItems: 'center',
     borderRadius: 10,
-    padding: 20
+    padding: 20,
+    marginBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
-  tituloCat:{
-    color: '#FFF',
-    fontSize: 18
-  }
+  retangulo2:{
+    backgroundColor: '#FF5A4D',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  retangulo3:{
+    backgroundColor: '#5698B0',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  retangulo4:{
+    backgroundColor: '#EB9560',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
 });
