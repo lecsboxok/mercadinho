48b0c170bcea4ead8a2e9a4664e6f27c1677727b
lecsboxok <133366992+lecsboxok@users.noreply.github.com>
2024-05-23 01:19:09 -0300

    subindo o carrossel

--- screens/principal.js
+++ screens/principal.js
@@ -1,6 +1,34 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { StyleSheet, Text, View, Image, Dimensions, FlatList } from 'react-native';
+
+const { width: viewportWidth } = Dimensions.get('window');
+
+const data = [
+  { url: require('../images/carrossel01.png') },
+  { url: require('../images/carrossel02.png') },
+  { url: require('../images/carrossel03.png') },
+  { url: require('../images/carrossel04.png') },
+];
+
+const CarouselItem = ({ item }) => (
+  <View style={styles.slide}>
+    <Image source={item.url} style={styles.imageCarousel} />
+    <Text style={styles.title}>{item.title}</Text>
+  </View>
+);
+
+const Carousel = () => (
+  <FlatList
+    data={data}
+    renderItem={({ item }) => <CarouselItem item={item} />}
+    keyExtractor={(item, index) => index.toString()}
+    horizontal
+    pagingEnabled
+    showsHorizontalScrollIndicator={false}
+    style={styles.carousel}
+  />
+);
 
 export function Principal() {
   return (
@@ -8,6 +36,7 @@ export function Principal() {
       <View style={styles.cabecalho}>
         <Image source={require('../images/logo.png')} style={styles.image} />
       </View>
+      <Carousel />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +45,30 @@ export function Principal() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFAF1',
+    padding: 20,
+  },
+  cabecalho: {
+    marginBottom: 20,
     alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 35.2,
+  },
+  carousel: {
+    flexGrow: 0,
+  },
+  slide: {
+    width: viewportWidth - 40,
+    marginHorizontal: 15,
+    borderRadius: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageCarousel: {
+    width: '100%',
+    height: 85,
+    resizeMode: 'cover',
   },
 });
